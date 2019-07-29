0ab33a4d4a6bc41776b5f016753cd0f34c98a17b
Kris de Rycke <derycke.kris@gmail.com>
2019-07-29 16:22:55 +0200

    update php file

--- randomData.php
+++ randomData.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 
     <?php
+header('Content-Type: application/json');
+header ('Access-Control-Allow-Origin: *');
+// $newArray= array();
+//     for ($i=0; $i<$_GET['numberX']; $i++){
+//         $number = rand(0,20);
+//         $formatJson = [$i,$number];
+//         array_push($newArray,$formatJson);
+//     }
 
 $newArray= array();
-    for ($i=0; $i<=9; $i++){
+    for ($i=0; $i<10; $i++){
         $number = rand(0,20);
         $formatJson = [$i,$number];
         array_push($newArray,$formatJson);
@@ -21,6 +20,3 @@ $newArray= array();
 echo json_encode ($newArray, JSON_PRETTY_PRINT);
 
     ?>
-    
-</body>
-</html>
